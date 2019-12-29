dddffea10bd4931cc2cfdf28cfad57b35bb818d7
Ignas Maslinskas <ignasmaslinskas@gmail.com>
2019-12-29 19:42:20 +0200

    Add odata resolver, use cache for index responses, add resolver priority
    Added odata resolver (by vradzius)
    Tried optimizing the index response retrieval by caching responses
    for 30minutes and also avoiding many initial requests to index endpoint
    to retrieve available services.
    Added configuration to set prioritized resolvers, which if not found fallback
    to using standard list.

--- src/providers/dotnet/nugetClient.js
+++ src/providers/dotnet/nugetClient.js
@@ -10,44 +10,101 @@ import { packageBaseAddressResolver } from './nugetResolvers/packageBaseAddressR
 import { registrationsBaseUrlResolver } from './nugetResolvers/registrationsBaseUrlResolver';
 import { searchAutocompleteServiceResolver } from './nugetResolvers/searchAutocompleteServiceResolver';
 import { searchQueryServiceResolver } from './nugetResolvers/searchQueryServiceResolver';
+import { odataClientResolver } from './nugetResolvers/odataClientResolver';
+
+const sleep = timeoutMs => new Promise(resolve => setTimeout(resolve, timeoutMs))
 
 // From https://docs.microsoft.com/en-us/nuget/api/overview
 // Sorted in order that we want to get them.
 const nugetServiceResolvers = [
-  { type: 'PackageBaseAddress/3.0.0', resolver: packageBaseAddressResolver },
-  { type: 'SearchAutocompleteService', resolver: searchAutocompleteServiceResolver },
-  { type: 'SearchAutocompleteService/3.0.0-beta', resolver: searchAutocompleteServiceResolver },
-  { type: 'SearchAutocompleteService/3.0.0-rc', resolver: searchAutocompleteServiceResolver },
   { type: 'RegistrationsBaseUrl', resolver: registrationsBaseUrlResolver },
   { type: 'RegistrationsBaseUrl/3.0.0-beta', resolver: registrationsBaseUrlResolver },
   { type: 'RegistrationsBaseUrl/3.0.0-rc', resolver: registrationsBaseUrlResolver },
   { type: 'RegistrationsBaseUrl/3.4.0', resolver: registrationsBaseUrlResolver },
   { type: 'RegistrationsBaseUrl/3.6.0', resolver: registrationsBaseUrlResolver },
+  { type: 'PackageBaseAddress/3.0.0', resolver: packageBaseAddressResolver },
+  { type: 'SearchAutocompleteService', resolver: searchAutocompleteServiceResolver },
+  { type: 'SearchAutocompleteService/3.0.0-beta', resolver: searchAutocompleteServiceResolver },
+  { type: 'SearchAutocompleteService/3.0.0-rc', resolver: searchAutocompleteServiceResolver },
   { type: 'SearchQueryService', resolver: searchQueryServiceResolver },
   { type: 'SearchQueryService/3.0.0-beta', resolver: searchQueryServiceResolver },
   { type: 'SearchQueryService/3.0.0-rc', resolver: searchQueryServiceResolver },
 ];
 
-async function getVersionResolverFromIndex(index) {
-  const httpRequest = require('request-light');
-  const indexResponse = await httpRequest.xhr({ url: index });
+// key: { response: "...", timestamp: "fetch date" }
+const responseCache = {};
+// How long the cached responses should be valid.
+const cacheValidForMs = 30 * 60 * 1000; // 30 mins.
 
-  if (indexResponse.status != 200) {
-    throw {
-      status: indexResponse.status,
-      responseText: indexResponse.responseText
-    };
+// key: timestamp
+const requestedAt = {};
+// How long to wait for other request to finish before starting new one.
+const requestTimeoutMs = 15 * 1000; // 15s
+
+async function getVersionResolverFromIndex(index) {
+  let response;
+  
+  if (responseCache[index] !== undefined) {
+    if (Date.now() - responseCache[index].timestamp < cacheValidForMs) {
+      console.log(`Using cached response for ${index}`);
+      response = responseCache[index].response;
+    }
   }
 
-  const indexData = JSON.parse(indexResponse.responseText);
+  while (response === undefined) {
+    // Try to do only 1 request for index response. 
+    if (requestedAt[index] === undefined || Date.now() - requestedAt[index] > requestTimeoutMs) {
+      requestedAt[index] = Date.now();
+      console.log(`Requesting for ${index}`);
+      const httpRequest = require('request-light');
+      const indexResponse = await httpRequest.xhr({ url: index });
+    
+      if (indexResponse.status != 200) {
+        throw {
+          status: indexResponse.status,
+          responseText: indexResponse.responseText
+        };
+      }
+  
+      response = indexResponse.responseText;
+      responseCache[index] = { response, timestamp: Date.now() };
+    } else {
+      console.log(`Waiting for ${index}`);
+      await sleep(250); // Sleep for 500ms before re-checking.
+      if (responseCache[index] !== undefined) {
+        response = responseCache[index].response;
+      }
+    }
+  }
 
-  // Go through the list with each resolver, as we prefer to get a resolver
-  // that is towards the start of the service resolver list.
-  // More efficient would be to just pick first acceptable resolver, but thats not ideal.
-  for (const serviceResolver of nugetServiceResolvers) {
-    for (const resource of indexData.resources) {
-      if (resource['@type'] === serviceResolver.type) {
-        return { index, url: resource['@id'], ...serviceResolver }; // return index + url + resolver
+  // Try to check if xml response, if yes, try OData resolver.
+  if (response.startsWith('<?xml version="1.0" encoding="utf-8"?>')) {
+    return { index, url: index, type: 'OData/2.0.0', resolver: odataClientResolver };
+  } else {
+    const indexData = JSON.parse(response);
+  
+    // Go through the list with each resolver, as we prefer to get a resolver
+    // that is towards the start of the service resolver list.
+    // More efficient would be to just pick first acceptable resolver, but thats not ideal.
+
+    // Search over priority resolver list.
+    for (const priorityResolver of appContrib.dotnetNuGetResolverPriority) {
+      const priorityResolverOb = nugetServiceResolvers.find(r => r.type === priorityResolver);
+      if (priorityResolverOb) {
+        for (const resource of indexData.resources) {
+          if (resource['@type'] === priorityResolverOb.type) {
+            return { index, url: resource['@id'], ...priorityResolverOb }; // return index + url + resolver
+          }
+        }
+      }
+    }
+
+    // Search over default resolvers.
+    for (const serviceResolver of nugetServiceResolvers) {
+      for (const resource of indexData.resources) {
+        if (resource['@type'] === serviceResolver.type) {
+          return { index, url: resource['@id'], ...serviceResolver }; // return index + url + resolver
+        }
       }
     }
   }
@@ -69,6 +126,7 @@ async function getAvailableResolvers() {
   try {
     const results = await Promise.all(resolved);
     const availableResolvers = results.filter(r => r.resolver !== undefined);
+    console.log('Available resolvers', availableResolvers)
 
     if (availableResolvers.length == 0) {
       throw { status: 404 };
